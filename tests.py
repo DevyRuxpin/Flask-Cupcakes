ab12b3f57ec7d531b5afa9333eea90a60af49bb2
DevyRuxpin <160616247+DevyRuxpin@users.noreply.github.com>
2025-03-04 14:05:41 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,14 +2,15 @@ from unittest import TestCase
 from app import app
 from models import db, Cupcake
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/cupcakes_test'
+# Use test database and don't clutter tests with SQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes_test'
 app.config['SQLALCHEMY_ECHO'] = False
+
+# Make Flask errors be real errors, rather than HTML pages with error info
 app.config['TESTING'] = True
 
-# Create tables
-with app.app_context():
-    db.drop_all()
-    db.create_all()
+db.drop_all()
+db.create_all()
 
 CUPCAKE_DATA = {
     "flavor": "TestFlavor",
@@ -30,20 +31,17 @@ class CupcakeViewsTestCase(TestCase):
 
     def setUp(self):
         """Make demo data."""
-        with app.app_context():
-            Cupcake.query.delete()
-            cupcake = Cupcake(**CUPCAKE_DATA)
-            db.session.add(cupcake)
-            db.session.commit()
-            self.cupcake = cupcake
+        Cupcake.query.delete()
+        cupcake = Cupcake(**CUPCAKE_DATA)
+        db.session.add(cupcake)
+        db.session.commit()
+        self.cupcake = cupcake
 
     def tearDown(self):
         """Clean up fouled transactions."""
-        with app.app_context():
-            db.session.rollback()
+        db.session.rollback()
 
     def test_list_cupcakes(self):
-        """Test getting list of cupcakes."""
         with app.test_client() as client:
             resp = client.get("/api/cupcakes")
             self.assertEqual(resp.status_code, 200)
@@ -61,7 +59,6 @@ class CupcakeViewsTestCase(TestCase):
             })
 
     def test_get_cupcake(self):
-        """Test getting a single cupcake."""
         with app.test_client() as client:
             url = f"/api/cupcakes/{self.cupcake.id}"
             resp = client.get(url)
@@ -78,24 +75,19 @@ class CupcakeViewsTestCase(TestCase):
             })
 
     def test_get_cupcake_missing(self):
-        """Test getting a cupcake that doesn't exist."""
         with app.test_client() as client:
             url = f"/api/cupcakes/99999"
             resp = client.get(url)
             self.assertEqual(resp.status_code, 404)
 
     def test_create_cupcake(self):
-        """Test creating a cupcake."""
         with app.test_client() as client:
             url = "/api/cupcakes"
             resp = client.post(url, json=CUPCAKE_DATA_2)
             self.assertEqual(resp.status_code, 201)
-
             data = resp.json
-            # don't know what ID we'll get, make sure it's an int
             self.assertIsInstance(data['cupcake']['id'], int)
             del data['cupcake']['id']
-
             self.assertEqual(data, {
                 "cupcake": {
                     "flavor": "TestFlavor2",
@@ -104,16 +96,13 @@ class CupcakeViewsTestCase(TestCase):
                     "image": "http://test.com/cupcake2.jpg"
                 }
             })
-
             self.assertEqual(Cupcake.query.count(), 2)
 
     def test_update_cupcake(self):
-        """Test updating a cupcake."""
         with app.test_client() as client:
             url = f"/api/cupcakes/{self.cupcake.id}"
             resp = client.patch(url, json=CUPCAKE_DATA_2)
             self.assertEqual(resp.status_code, 200)
-
             data = resp.json
             self.assertEqual(data, {
                 "cupcake": {
@@ -124,31 +113,26 @@ class CupcakeViewsTestCase(TestCase):
                     "image": "http://test.com/cupcake2.jpg"
                 }
             })
-
             self.assertEqual(Cupcake.query.count(), 1)
 
     def test_update_cupcake_missing(self):
-        """Test updating a cupcake that doesn't exist."""
         with app.test_client() as client:
             url = f"/api/cupcakes/99999"
             resp = client.patch(url, json=CUPCAKE_DATA_2)
             self.assertEqual(resp.status_code, 404)
 
     def test_delete_cupcake(self):
-        """Test deleting a cupcake."""
         with app.test_client() as client:
             url = f"/api/cupcakes/{self.cupcake.id}"
             resp = client.delete(url)
             self.assertEqual(resp.status_code, 200)
-
             data = resp.json
             self.assertEqual(data, {"message": "Deleted"})
-
             self.assertEqual(Cupcake.query.count(), 0)
 
     def test_delete_cupcake_missing(self):
-        """Test deleting a cupcake that doesn't exist."""
         with app.test_client() as client:
             url = f"/api/cupcakes/99999"
             resp = client.delete(url)
             self.assertEqual(resp.status_code, 404)
+
